428b93488926bf3f4993c8bfbd7630f6334cbb57
Liam Brenner <liam.brenner@gmail.com>
2015-07-03 23:58:28 +1000

    Set groundwork for further improvement

--- utils/youtube_data.py
+++ utils/youtube_data.py
@@ -4,5 +4,15 @@ from local import YOUTUBE_API_KEY
 
 def get_first_video(name):
     youtube_search = requests.get(
-        'https://www.googleapis.com/youtube/v3/search/' +
+        'https://www.googleapis.com/youtube/v3/search?part' +
         '')
+    return ''  # The initial results video ID (passed to get related vid)
+
+
+def get_related_to_video(name):
+    youtube_search = requests.get(
+        'https://www.googleapis.com/youtube/v3/search?part' +
+        '' +
+        '&relatedToVideoId=' + get_first_video(name))
+
+    return ''  # Return the related vid url or id?
