a80ac5aac2c71365226b4e920e7a83990eb14558
Liam Brenner <liam.brenner@gmail.com>
2015-07-04 11:19:46 +1000

    Use urlencoding, handle errors

--- utils/tvdb_data.py
+++ utils/tvdb_data.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import requests
+import urllib
 import xml.etree.ElementTree as ET
 from optparse import OptionParser
 from local import TVDB_API_KEY
@@ -7,16 +8,21 @@ from xml2json import xml2json
 
 
 def get_series_id(series_name):
-    tvbd_request = requests.get(
+    tvbd_request_str = (
         'http://thetvdb.com/api/' +
         'GetSeries.php?seriesname=' +
-        str(series_name))
+        str(urllib.quote(series_name)))
+
+    tvbd_request = requests.get(tvbd_request_str)
 
     response = tvbd_request.content
     tree = ET.fromstring(response)
     series = tree.find('Series')
-    series_id = series.find('seriesid')
-    return series_id.text
+    if series is not None:
+        series_id = series.find('seriesid')
+        return series_id.text
+    else:
+        print 'Query returned nothing'
 
 
 def return_json(data):
@@ -26,26 +32,28 @@ def return_json(data):
 
 def get_series_episode(series_name, se, ep):
     series_id = get_series_id(series_name)
-    tvbd_request = requests.get(
-        'http://thetvdb.com/api/' +
-        API_KEY +
-        '/series/' +
-        series_id +
-        '/default/' +
-        str(se) +
-        '/' +
-        str(ep))
-    response = tvbd_request.content
-    print return_json(response)
+
+    if series_id is not None:
+        tvbd_request = requests.get(
+            'http://thetvdb.com/api/' +
+            TVDB_API_KEY +
+            '/series/' +
+            series_id +
+            '/default/' +
+            str(se) +
+            '/' +
+            str(ep))
+        response = tvbd_request.content
+        print return_json(response)
 
 
 class ParserOptions():
     parser = OptionParser(
         description="Pulls data from tvdb!",
         prog='tvdb_data',
-        usage='./%prog.py -n series_name -s season_num -e episode_num '
+        usage='./%prog.py -n "series_name" -s season_num -e episode_num '
         )
-    parser.add_option("-n", "--name", action='store', dest='name')
+    parser.add_option("-n", "--name", action='store', dest='name', help='Wrapped in quotes')
     parser.add_option("-s", "--season", action='store', dest='season')
     parser.add_option("-e", "--episode", action='store', dest='episode')
 
