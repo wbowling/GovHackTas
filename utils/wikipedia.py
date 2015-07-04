762eff8aeb9b29d5ac2734abf03631bc913d8cca
Liam Brenner <liam.brenner@gmail.com>
2015-07-04 14:29:00 +1000

    Finished wikipedia script, pulls first p tag from relevant series

--- utils/wikipedia.py
+++ utils/wikipedia.py
@@ -1,9 +1,18 @@
 import requests
 import urllib
+import re
 import json
+from bs4 import BeautifulSoup
 from optparse import OptionParser
 
 base_url = 'https://en.wikipedia.org/w/api.php?'
+
+
+def subit(msg):
+    # Use the below if the string is multiline
+    # subbed = re.compile("(<.*?>)" re.DOTALL).sub("(<hh  >t", msg)
+    subbed = re.sub("(\[.*?\])", "", msg)
+    return subbed
 
 
 def get_wikipedia_page_id(title):
@@ -29,23 +38,27 @@ def get_wikipedia_info(title):
     page_url = get_wikipedia_page_id(title)
     request = (page_url)
     wikipedia_response = requests.get(request).content
-    print wikipedia_response
-'''
+    soup = BeautifulSoup(wikipedia_response)
+    text_div = soup.find(id='mw-content-text')
+    div_children = text_div.children
+    for tag in div_children:
+        if tag.name == 'p':
+            print subit(tag.get_text())
+            break
+
+
 class ParserOptions():
     parser = OptionParser(
-        description="Searches wikipedia for a ",
-        prog='yt_query',
-        usage='./%prog.py -q "query" '
+        description="Searches wikipedia for a series/movie title ",
+        prog='wiki_query',
+        usage='./%prog.py -t "title" '
         )
-    parser.add_option("-q", "--query", action='store', dest='query', help='Wrapped in quotes')
+    parser.add_option("-t", "--title", action='store', dest='title', help='Wrapped in quotes')
 
     options, arguments = parser.parse_args()
 
-    if not options.query:
-        parser.error('Query not provided.')
+    if not options.title:
+        parser.error('Title not provided.')
 
-    query = str(options.query)
-    title = ''
-    #get_wikipedia_info(title)
-'''
-get_wikipedia_info('Cats')
+    title = str(options.title)
+    get_wikipedia_info(title)
