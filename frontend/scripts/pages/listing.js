f7b6c0cdd5c6811c1116617cb64fde6543e7cd4f
Liam Brenner <liam.brenner@gmail.com>
2015-07-05 14:20:04 +1000

    Added left and right sliders on year selecters

--- frontend/scripts/pages/listing.js
+++ frontend/scripts/pages/listing.js
@@ -34,6 +34,25 @@ var Listing = React.createClass({
             year: e.target.value
         })
     },
+    addYear(e){
+        if (this.state.year < 2012) {
+        this.setState({
+            year: parseInt(this.state.year) + 1
+        })
+        var slider = this.refs.yearSlider.getDOMNode()
+        slider.value = this.state.year
+        this.sliderFinished({target:slider})
+    }},
+
+    removeYear(e){
+        if (this.state.year > 1977) {
+        this.setState({
+            year: parseInt(this.state.year) - 1
+        })
+        var slider = this.refs.yearSlider.getDOMNode()
+        slider.value = this.state.year
+        this.sliderFinished({target:slider})
+    }},
 
     shouldComponentUpdate(nextProps, nextState){
         return true;//this.state.showList !== nextState.showList;
@@ -50,7 +69,9 @@ var Listing = React.createClass({
             <div className="listing">
                 <span className="listing--year">{this.state.year}</span>
                 <br/>
+                <button className='listing--control' onClick={this.removeYear}>&#x25C0;</button>
                 <input defaultValue={this.state.year} min="1978" max="2011" type="range" step="1" ref="yearSlider" onChange={this.sliderMove} onMouseUp={this.sliderFinished}/>
+                <button className='listing--control' onClick={this.addYear}>&#x25B6;</button>
                 { listingItems }
 
             </div>
